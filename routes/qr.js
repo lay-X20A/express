88f7eb35a1972de05b4f05bfc0a62ffb59402da4
lay-X20A <ran0maru0@gmaill.com>
2023-12-07 09:20:58 +0900

    add qr api

--- routes/qr.js
+++ routes/qr.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const request = require('request');
 
-router.get('/', async (req, res) => {
-    request('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.writeHead(200, {'Content-Type': 'image/png'});
-            res.end(body, 'binary');
-}
+router.get('/', (req, res) => {
+    const apiUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example';
+
+    request.get({ url: apiUrl, encoding: null }, (error, response, body) => {
+        if (!error && response.statusCode === 200 && response.headers['content-type'] === 'image/png') {
+            res.contentType('image/png').send(body);
+        } else {
+            console.error(error || 'Invalid response');
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
 });
-})
+
 module.exports = router;
