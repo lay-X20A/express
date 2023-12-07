30cb7e04742cd0dcfd3d9c1c734257d7ab83945c
lay-X20A <ran0maru0@gmaill.com>
2023-12-07 09:06:32 +0900

    add qr api

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
-var notes_from_bRouter = require('./routes/notes_from_b');
+var qrRouter = require('./routes/qr');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
-app.use('/cat', catRouter);
-app.use('/notes_from_b', notes_from_bRouter);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+app.use('/cat',catRouter);
+app.use('/qr',qrRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
--- routes/cat.js
+++ routes/cat.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+
 router.get('/', async (req, res) => {
-request('https://api.thecatapi.com/v1/images/search', function (error, response,
-body) {
-if (!error && response.statusCode == 200) {
-const data = JSON.parse(body);
-res.json(data);
-}
+    request('https://dog.ceo/api/breeds/image/random', (error, response, body) => {
+        if (error) {
+            console.error('エラー:', error);
+            res.status(500).json({ error: '内部サーバーエラー' });
+        } else if (response.statusCode !== 200) {
+            console.error('ステータスコード:', response.statusCode);
+            res.status(500).json({ error: '犬の画像の取得に失敗しました。' });
+        } else {
+            try {
+                const data = JSON.parse(body);
+                res.json(data);
+            } catch (parseError) {
+                console.error('解析エラー:', parseError);
+                res.status(500).json({ error: '応答の解析に失敗しました。' });
+            }
+        }
+    });
 });
-})
+
 module.exports = router;
